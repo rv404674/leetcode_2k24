842b34291a1261c35875fa47a2a67557eb005758
Rahul Verma <rv404674@gmail.com>
2024-06-12 19:34:50 +0530

    Util methods

---
+++ src/UtilMethods.java
@@ -0,0 +1,8 @@
+public class UtilMethods {
+    public void allMethods() {
+        // NOTE: Method 1
+        // HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        // graph.computeIfAbsent(root.val, k -> new ArrayList<>()).add(root.left.val);
+
+    }
+}
